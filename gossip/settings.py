0f11850ab90729515247cbf21ee67ea1f628c541
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-31 21:25:06 +0300

    updated the secretkey

--- gossip/settings.py
+++ gossip/settings.py
@@ -27,6 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+SECRET_KEY=config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 MODE=config("MODE", default="dev")
 SECRET_KEY=config('SECRET_KEY')
